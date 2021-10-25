45d897ab2a009a097605294e28d314d07ff0844a
JM <j.maxwell.alan@gmail.com>
2021-10-24 23:19:18 -0400

    basic homepage styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function App(): JSX.Element {
   return (
-    <div>
-      <span className='font-extrabold text-3xl'>
-        Hello React
+    <div className='bg-black h-screen w-screen'>
+      <span className='font-extrabold text-3xl text-white'>
+        Hello World!
       </span>
     </div>
   );
