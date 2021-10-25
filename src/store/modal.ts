a2c312c7d338b5862e370a4c3fed7318b880eeab
JM <j.maxwell.alan@gmail.com>
2021-10-24 23:14:58 -0400

    remove modal mooring from reducer

--- src/store/modal.ts
+++ src/store/modal.ts
@@ -2,17 +2,14 @@ import { JSXElementConstructor } from 'react';
 
 const SHOW: string = 'modal/SHOW';
 const HIDE: string = 'modal/HIDE';
-const MOORING: string = 'modal/MOORING';
 const CURRENT: string = 'modal/CURRENT';
 
 export type modalState = {
   display: boolean,
-  mooring: JSX.IntrinsicElements;
   Current: JSXElementConstructor<any>,
 };
 
 type modalAction = {
-  mooring?: JSX.IntrinsicElements;
   Current?: JSXElementConstructor<any>,
   type: string;
 };
@@ -25,19 +22,14 @@ export const HideModal = (): modalAction => ({
   type: HIDE
 });
 
-export const SetModalMooring = (mooring: JSX.IntrinsicElements): modalAction => ({
-  type: MOORING,
-  mooring
-});
-
 export const SetModalCurrent = (Current: JSXElementConstructor<any>): modalAction => ({
   type: CURRENT,
   Current
 });
 
 export default function reducer(
-  state: modalState = { display: false, Current: null, mooring: null },
-  { type, mooring, Current }: modalAction
+  state: modalState = { display: false, Current: null },
+  { type, Current }: modalAction
 ): modalState {
   switch (type) {
     case SHOW:
@@ -49,11 +41,6 @@ export default function reducer(
       return {
         ...state,
         display: false
-      };
-    case MOORING:
-      return {
-        ...state,
-        mooring
       };
     case CURRENT:
       return {
